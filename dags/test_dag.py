a1395f5b41f07deefa6aa359c36604598421fbc6
mehmet-kavrut <mehmetkavrut@gmail.com>
2025-09-27 22:12:58 +0200

    Test CI/CD workflow with dummy DAG

---
+++ dags/test_dag.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello_world():
+    print("Hello from test DAG!")
+
+with DAG(
+    "test_dag",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="@once",
+    catchup=False,
+) as dag:
+    task = PythonOperator(
+        task_id="hello_world_task",
+        python_callable=hello_world
+    )
